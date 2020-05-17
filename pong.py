89397942e088e6437fe6593649b904717fa448e3
JaeKyung Sung <jaekyungsung@woodstock.ac.in>
2020-05-17 19:31:19 +0900

    Set up basics (screen, title, background color, window size)

--- pong.py
+++ pong.py
@@ -1,3 +1,18 @@
-import pygame
 import turtle
 
+# Create Window
+wn = turtle.Screen()
+
+# Title
+wn.title("Pong")
+
+# Background color
+wn.bgcolor("black")
+
+# Size of the window
+wn.setup(width=800, height=600)
+
+# Stops window from refreshing automatically
+wn.tracer(0)
+
+# Main game Loop
