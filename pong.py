ddc367da0574a71a500f64b29ef3f8089d80bf32
JaeKyung Sung <jaekyungsung@woodstock.ac.in>
2020-05-17 19:39:11 +0900

    Set up paddle_a

--- pong.py
+++ pong.py
@@ -15,6 +15,25 @@ wn.setup(width=800, height=600)
 # Stops window from refreshing automatically
 wn.tracer(0)
 
+# Paddle A
+paddle_a = turtle.Turtle()
+# speed animation (A)
+paddle_a.speed(0)
+# shape (A)
+paddle_a.shape("square")
+# Color (A)
+paddle_a.color("white")
+# No drawing line (A)
+paddle_a.penup()
+# Set up coordinate (A)
+paddle_a.goto(-350, 0)
+# Set up size in ratio from 20px by 20px (A)
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+
+
+
+# Paddle B
+
 # Main game Loop
 while True:
     # Update screen
